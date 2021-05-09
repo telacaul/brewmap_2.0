222b8886bd818c827a608903442457e3e1411fd9
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-05-09 14:59:25 -0700

    create blogroutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+const homeRoutes = require('./home-routes');
+const blogRoutes = require('./blog-routes');
 
 
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
+router.use('/blog', blogRoutes);
 
 
 router.use((req, res) => {
