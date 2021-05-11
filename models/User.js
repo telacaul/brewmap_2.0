4e205a25a7937fd1453be901c914c4078c373d23
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-05-11 09:29:58 -0700

    add files and data to get server running

--- models/User.js
+++ models/User.js
@@ -1,14 +1,47 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
-
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.pasword);
+    }
+}
 
 User.init (
     {
         // data goes here
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true
+        }, 
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        },
+        
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         freezeTableName: true,
         underscored: true,
